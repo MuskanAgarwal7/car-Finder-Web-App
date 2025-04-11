a94d055f1197157108e4b2f02de4c2e09ffccdf7
MuskanAgarwal7 <muskaninjhansi@gmail.com>
2025-04-11 23:23:17 +0530

    Fix: Folder casing from Components to components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CarDetails from "./pages/CarDetails";
 import Wishlist from "./pages/Wishlist";
-import NotFound from "./Components/NotFound";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
